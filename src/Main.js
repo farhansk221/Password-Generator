81bb746c65769dce6ea9813b9d0958945d30e2a0
farhansk221 <2024.mohd.farhan.shaikh@ves.ac.in>
2025-07-11 21:07:59 +0530

    Second commit

--- src/Main.js
+++ src/Main.js
@@ -31,7 +31,7 @@ function Main() {
 
     const copyPass=()=>{
         window.navigator.clipboard.writeText(password)
-        alert("copied")
+        copyRef.current.select()
     }
 
     return (
@@ -39,11 +39,11 @@ function Main() {
             <div className='w-full max-w-md mx-auto shadow-md rounded-lg px-4 py-3 my-8    bg-gray-800 text-orange-500'>
                 <h2 className='text-white text-center my-3'>Password Generator</h2>
                 <div className='flex shadonw rounded-lg overflow-hidden mb-4'>
-                    <input type="text" value={password} className='outline-none w-full py-1 px-3 ' placeholder='Password' readOnly></input>
+                    <input ref={copyRef} type="text" value={password} className='outline-none w-full py-1 px-3 ' placeholder='Password' readOnly></input>
                 </div>
                 <button onClick={genpassword} className="bg-blue-700 mx-3 text-white px-3 py-1 rounded">Generate</button>
 
-                <button ref={copyRef} onClick={copyPass} className='outline-none bg-blue-700 text-white px-3 py-0.5 shrink-0'>Copy</button>
+                <button onClick={copyPass} className='outline-none bg-blue-700 text-white px-3 py-0.5 shrink-0'>Copy</button>
                 <div className='flex text-sm gap-x-2'>
                     <div className='flex items-center gap-x-1'>
                         <input type='range' min={6} max={100} value={length} name='' id='' onChange={(e) => setlength(e.target.value)}></input>
